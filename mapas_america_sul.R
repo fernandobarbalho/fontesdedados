f94dfbda6bf094a90e7d069bff3650cf58dbfb18
Fernando Almeida Barbalho <40247971+fernandobarbalho@users.noreply.github.com>
2024-03-22 17:08:30 -0300

    Melhoras no mapa da América do Sul

--- mapas_america_sul.R
+++ mapas_america_sul.R
@@ -44,15 +44,15 @@ total_pib_america_sul<-
 
 
 
-transformed_data <- read_csv("transformed_data.csv")
+transformed_data <- read_csv("percentual_pib_por_ano.csv")
 
 # Criar um vetor com os códigos de países
-codigos_paises <- c("ARG", "BOL", "BRA", "CHL", "COL", "ECU", "PER", "PRY", "URY", "VEN")
+codigos_paises <- c("ARG", "BOL", "BRA", "CHL", "COL", "ECU", "PER", "PRY", "URY", "VEN","SUR","GUY")
 
 # Tabela de correspondência entre códigos de países e códigos de duas letras ISO
 tabela_iso <- data.frame(
-  codigo_pais = c("ARG", "BOL", "BRA", "CHL", "COL", "ECU", "PER", "PRY", "URY", "VEN"),
-  codigo_iso = c("AR", "BO", "BR", "CL", "CO", "EC", "PE", "PY", "UY", "VE"),
+  codigo_pais = c("ARG", "BOL", "BRA", "CHL", "COL", "ECU", "PER", "PRY", "URY", "VEN","SUR","GUY"),
+  codigo_iso = c("AR", "BO", "BR", "CL", "CO", "EC", "PE", "PY", "UY", "VE","SR","GY"),
   stringsAsFactors = FALSE
 )
 
@@ -63,12 +63,7 @@ dados_pib<-
 transformed_data %>%
   inner_join(tabela_iso) %>%
   mutate(iso_a2= codigo_iso) %>%
-  filter(ano %in% c(1960, 2023)) %>%
-  bind_rows(tibble( codigo_pais= "GUY", ano = 1960, percentual_pib= NA, iso_a2 ="GY")) %>%
-  bind_rows(tibble( codigo_pais= "SUR", ano = 1960, percentual_pib= NA, iso_a2 ="SR")) %>%
-  bind_rows(tibble( codigo_pais= "GUY", ano = 2023, percentual_pib= NA, iso_a2 ="GY")) %>%
-  bind_rows(tibble( codigo_pais= "SUR", ano = 2023, percentual_pib= NA, iso_a2 ="SR")) 
-
+  filter(ano %in% c(1960, 2023)) 
 
 southamerica<-
   world %>%
@@ -78,6 +73,12 @@ southamerica<-
 france<-
   world %>%
   filter(iso_a2 == "FR")
+
+central_america<-
+  world %>%
+  filter(subregion == "Central America")
+
+
 
 
 
@@ -106,13 +107,15 @@ southamerica%>%
   filter(!is.na(ano)) %>%
   #mutate(ano = 1960) %>%
   ggplot() +
-  geom_sf(aes(fill=percentual_pib*100)) +
-  geom_sf(data=france) +
-  geom_label(data= dados_textos,aes(x=lon, y=lat,label= str_wrap(paste0(codigo_pais," ",round(percentual_pib*100,1),"%"),3)), 
-                  color = "black", 
-                  fontface = "bold", 
-                  size = 2.5) +
-  geom_label(data= dados_textos,aes(x=-50, y=-40,label= ano), 
+  geom_sf(aes(fill=percentual_pib*100),  lwd=0.1, color ="#DCDCDC") +
+  geom_sf(data=france, fill = "#C0C0C0") +
+  geom_sf(data=central_america, fill= "#C0C0C0" ) +
+  geom_text(data= dados_textos,
+            aes(x=lon, y=lat,label= str_wrap(paste0(codigo_pais," ",round(percentual_pib*100,1),"%"),3)), 
+            color = "black",
+            fontface = "bold",
+            size = 2.5) +
+  geom_label(data= dados_textos,aes(x=-40, y=-60,label= ano), 
              color = "black", 
              fontface = "bold", 
              size = 6) +
@@ -121,11 +124,12 @@ southamerica%>%
   coord_sf(xlim = c(-82,-35), ylim=c(-60,15))+
   theme_void() +
   theme(
-    panel.background = element_rect(fill="#0077be"),
+    panel.background = element_rect(fill="#57A8C2"), #
     strip.text = element_blank()
   ) +
   labs(
     title = "Participação no PIB gerado na América do Sul",
+    caption = "Fonte: Maddison Project Database e FMI",
     subtitle = "Participação por país",
     fill= str_wrap("(%)", 10)
   ) +
