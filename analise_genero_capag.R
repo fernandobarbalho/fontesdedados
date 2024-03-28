607c8c20073659c317b0005dcf026ca125677a14
Fernando Almeida Barbalho <40247971+fernandobarbalho@users.noreply.github.com>
2024-03-28 16:50:19 -0300

    Testes estatísticos sobre gênero

--- analise_genero_capag.R
+++ analise_genero_capag.R
@@ -1,5 +1,9 @@
 library(tidyverse)
 library(ggrepel)
+library(lubridate)
+library(caret)
+library(rattle)
+
 capag_sexo <- read_csv("capag_sexo.csv")
 
 capag_sexo$saldo <- capag_sexo$saldo/1000
@@ -103,7 +107,7 @@ fab<-
 
 capag_sexo_cnae %>%
   filter(sexo %in% c(1, 3),
-         cnae_2_secao %in% c("P","G")) %>%
+         cnae_2_secao %in% c("P","G","I","Q")) %>%
   mutate(sexo = ifelse(sexo == 1, "Masculino", "Feminino")) %>%
   pivot_wider(names_from = sexo,
               values_from = saldo) %>%
@@ -119,5 +123,49 @@ capag_sexo_cnae %>%
 
 
 
-         
+dados_modelo<-
+  capag_sexo_cnae  %>%
+  filter(sexo %in% c(1, 3)) %>%
+  filter(mes %in% c(2,11)) %>%
+  mutate(sexo = ifelse(sexo == 1, "Masculino", "Feminino")) %>%
+  pivot_wider(names_from = sexo,
+              values_from = saldo) %>%
+  filter(Feminino>0 & Masculino>0) %>%
+  mutate(saldo_pro_f = Feminino- Masculino,
+         prop_saldo_pro_f = saldo_pro_f/Feminino,
+         sexo_contratacao = ifelse(saldo_pro_f>0,"Feminino","Masculino")) %>%
+  arrange(desc(saldo_pro_f))
+
+
+modelo_lm<-
+lm(saldo_pro_f~ as.factor(mes)+cnae_2_secao, data= dados_modelo )
+
+summary(modelo_lm)
+
+control_dt <- trainControl(method="cv")
+seed <- 1972
+set.seed(seed)
+
+
+
+dt_model <- train(sexo_contratacao~ as.factor(mes)+cnae_2_secao,
+                  data=dados_modelo, 
+                  method="rpart", 
+                  trControl=control_dt)
+
+fancyRpartPlot(dt_model$finalModel)
+
+
+capag_sexo_cnae %>%
+  filter(sexo %in% c(1, 3),
+         cnae_2_secao %in% c("P","G","I","Q")) %>%
+  mutate(sexo = ifelse(sexo == 1, "Masculino", "Feminino"),
+         mes = as.factor(mes)) %>%
+  summarise(soma_saldo = sum(saldo),
+            .by = c(mes,sexo,cnae_2_secao)) %>%
+  ggplot(aes(x=mes, y= soma_saldo)) +
+  geom_col(aes(fill= sexo), position = "dodge") +
+  facet_wrap(cnae_2_secao~.)
+
+
 
