c704f0e75bd484d239ad02cbea1eb0b5f88297fd
Fernando Barbalho <fbarbalho@hotmail.com>
2024-04-23 11:17:47 -0300

    Tratamento de tipo de viagem

--- viagens_ministros.R
+++ viagens_ministros.R
@@ -1,6 +1,13 @@
 library(tidyverse)
 library(colorspace)
 library(geobr)
+library(readxl)
+
+#ETL
+
+de_para_nomes <- read_excel("de_para_nomes.xlsx")
+
+de_para_nomes$nome_longo[5] <-"ANDRE LUIZ CARVALHO RIBEIRO"
 
 sedes<- geobr::read_municipal_seat()
 estados<- geobr::read_state()
@@ -12,6 +19,36 @@ viagens_ministros <- read_csv("viagens_ministros.csv",
 viagens_ministros <- janitor::clean_names(viagens_ministros)
 
 
+ministros_lula <- read_csv("ministros_lula.csv")
+
+ministros_lula <- janitor::clean_names(ministros_lula)
+
+ministros_lula<- ministros_lula[-15,]
+
+ministros_lula$nome[28]<- "Márcio França"
+
+sort(ministros_lula$nome)
+
+sort(unique(viagens_ministros$nome))
+
+tibble(nome_curto = sort(ministros_lula$nome), nome_longo = c(sort(unique(viagens_ministros$nome)),NA)) %>%
+  readr::write_csv("tabela_nomes.csv")
+
+names(ministros_lula)[2] <- "nome_curto_original"
+
+ministros_lula<-
+ministros_lula %>%
+  inner_join(de_para_nomes)
+
+
+ministros_lula<-
+ministros_lula %>%
+  separate(partido, into = c("partido","uf"), sep = "/")
+
+
+viagens_ministros<-
+viagens_ministros %>%
+  separate(destinos, into = c("cidade","uf_destino"), sep = "/", remove = FALSE)  
 
 
 viagens_ministros %>%
@@ -32,11 +69,12 @@ viagens_ministros %>%
   )
 
 
+
+
 sedes_sel<-
   viagens_ministros %>%
   filter(situacao == "Realizada",
          stringr::str_detect(destinos,"[,]", negate=TRUE)) %>%
-  separate(destinos, into = c("cidade","uf"), sep = "/") %>%
   mutate(cidade = str_to_title(cidade)) %>%
   summarise(quantidade = n(),
             .by = c(cidade,uf)) %>%
@@ -70,6 +108,15 @@ top_6_cidades<-
   slice_max(order_by = quantidade, n=6))$destinos
   
 
+top_20_cidades<-
+  (viagens_ministros %>%
+     filter(situacao == "Realizada") %>%
+     summarise(quantidade = n(),
+               .by = destinos) %>%
+     ungroup() %>%
+     slice_max(order_by = quantidade, n=20))$destinos
+
+
 viagens_ministros %>%
   filter(situacao == "Realizada",
          destinos %in% top_6_cidades) %>%
@@ -90,3 +137,77 @@ viagens_ministros %>%
   facet_wrap(destinos ~.)
 
   
+
+
+
+viagens_ministros %>%
+  rename(nome_longo = nome) %>%
+  inner_join(ministros_lula) %>%
+  mutate(tipo_viagem = case_when(
+    tipo_viagem == "Internacional" ~ "Internacional",
+    str_detect(destinos, uf, negate = TRUE) ~ "Outros destinos",
+    str_detect(destinos, uf) & str_count(destinos, "/") > 1 ~ "Misto",
+    str_detect(destinos, uf) & str_count(destinos, "/") == 1 ~ "Residência eleitoral"
+  ))%>%
+  mutate(tipo_viagem = factor(tipo_viagem, levels= c("Internacional", "Residência eleitoral", "Misto","Outros destinos"))) %>%
+  summarise( quantidade = n(),
+             .by = c(nome_curto_original,tipo_viagem)) %>%
+  mutate(nome_curto_original = fct_reorder(nome_curto_original, quantidade, sum )) %>%
+  ggplot() +
+  geom_col(aes(x= quantidade, y= nome_curto_original, fill= tipo_viagem)) +
+  scale_fill_discrete_qualitative(palette = "Dark 2") +
+  theme_light() +
+  theme(
+    panel.grid = element_blank(),
+    panel.background = element_rect(fill = "black")
+  )
+
+
+viagens_ministros %>%
+  rename(nome_longo = nome) %>%
+  inner_join(ministros_lula) %>%
+  mutate(tipo_viagem = case_when(
+    tipo_viagem == "Internacional" ~ "Internacional",
+    str_detect(destinos, uf, negate = TRUE) ~ "Outros destinos",
+    str_detect(destinos, uf) & str_count(destinos, "/") > 1 ~ "Misto",
+    str_detect(destinos, uf) & str_count(destinos, "/") == 1 ~ "Residência eleitoral"
+  ))%>%
+  mutate(tipo_viagem = factor(tipo_viagem, levels= c("Internacional", "Residência eleitoral", "Misto","Outros destinos"))) %>%
+  summarise( quantidade = n(),
+             .by = c(nome_curto_original,tipo_viagem)) %>%
+  mutate(nome_curto_original = fct_reorder(nome_curto_original, quantidade, sum )) %>%
+  ggplot() +
+  geom_col(aes(x= quantidade, y= nome_curto_original, fill= tipo_viagem), position = "fill") +
+  scale_fill_discrete_qualitative(palette = "Dark 2") +
+  theme_light() +
+  theme(
+    panel.grid = element_blank(),
+    panel.background = element_rect(fill = "black")
+  )
+
+
+
+viagens_ministros %>%
+  filter(destinos %in% top_20_cidades) %>%
+  rename(nome_longo = nome) %>%
+  inner_join(ministros_lula) %>%
+  mutate(tipo_viagem = case_when(
+    tipo_viagem == "Internacional" ~ "Internacional",
+    str_detect(destinos, uf, negate = TRUE) ~ "Outros destinos",
+    str_detect(destinos, uf) & str_count(destinos, "/") > 1 ~ "Misto",
+    str_detect(destinos, uf) & str_count(destinos, "/") == 1 ~ "Residência eleitoral"
+  ))%>%
+  mutate(tipo_viagem = factor(tipo_viagem, levels= c("Internacional", "Residência eleitoral", "Misto","Outros destinos"))) %>%
+  summarise( quantidade = n(),
+             .by = c(destinos,tipo_viagem)) %>%
+  mutate(destinos = fct_reorder(destinos, quantidade, sum )) %>%
+  ggplot() +
+  geom_col(aes(x= quantidade, y= destinos, fill= tipo_viagem), position = "fill") +
+  scale_fill_discrete_qualitative(palette = "Dark 2") +
+  theme_light() +
+  theme(
+    panel.grid = element_blank(),
+    panel.background = element_rect(fill = "black")
+  )
+
+
