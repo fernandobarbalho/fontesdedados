afc43b5bf15b0b93736f531b260ca7e3bf6b229d
Fernando Barbalho <fbarbalho@hotmail.com>
2024-09-13 15:29:33 -0300

    análises exploratórias com China e Argentina

--- biotrade_unctad.R
+++ biotrade_unctad.R
@@ -131,13 +131,93 @@ brazil_full<-
   unctad %>%
   filter(economy_label == "Brazil")
 
+brazil_full <- as_tibble(brazil_full)
 
 brazil_full %>% saveRDS("brazil_full.rds")
 
+
 brazil_full %>%
   filter(partner < 1400,
-         product_label == "B_TOT", 
+         partner >0,
+         product == "B_TOT", 
          flow == 14) %>%
   summarise(total = sum(us_dollars_at_current_prices_in_thousands),
             .by =partner_label) %>%
-  slice_max(order_by = partner_label, n= 10)
+  slice_min(order_by = total, n= 10)
+
+
+brazil_full %>%
+  filter(partner < 1400,
+         partner >0,
+         product == "B_TOT", 
+         flow == 14) %>%
+  summarise(total = sum(us_dollars_at_current_prices_in_thousands),
+            .by =c(partner_label, partner)) %>%
+  slice_max(order_by = total, n= 10)
+
+
+###Exercícios Artentina
+
+brazil_full %>%
+  filter(partner == 32,
+         str_length(product) == 3,
+         flow == 14) %>%
+  summarise(total = sum(us_dollars_at_current_prices_in_thousands),
+            .by = product_label) %>%
+  slice_min(order_by = total, n= 10)
+
+
+
+brazil_full %>%
+  filter(partner == 32,
+         str_length(product) == 4,
+         str_starts(product, "B_B"),
+         flow == 14) %>%
+  summarise(total = sum(us_dollars_at_current_prices_in_thousands),
+            .by = c(product_label, product)) %>%
+  slice_min(order_by = total, n= 10)
+
+
+
+brazil_full %>%
+  filter(partner == 32,
+         str_length(product) == 6,
+         str_starts(product, "B_BJ"),
+         flow == 14) %>%
+  summarise(total = sum(us_dollars_at_current_prices_in_thousands),
+            .by = c(product_label, product)) %>%
+  slice_min(order_by = total, n= 10)
+
+
+#Exercícios China
+
+brazil_full %>%
+  filter(partner == 156,
+         str_length(product) == 3,
+         flow == 14) %>%
+  summarise(total = sum(us_dollars_at_current_prices_in_thousands),
+            .by =c( product_label, product)) %>%
+  slice_max(order_by = total, n= 10)
+
+
+
+brazil_full %>%
+  filter(partner == 32,
+         str_length(product) == 4,
+         str_starts(product, "B_D"),
+         flow == 14) %>%
+  summarise(total = sum(us_dollars_at_current_prices_in_thousands),
+            .by = c(product_label, product)) %>%
+  slice_max(order_by = total, n= 10)
+
+
+
+brazil_full %>%
+  filter(partner == 32,
+         str_length(product) == 6,
+         str_starts(product, "B_DR"),
+         flow == 14) %>%
+  summarise(total = sum(us_dollars_at_current_prices_in_thousands),
+            .by = c(product_label, product)) %>%
+  slice_max(order_by = total, n= 10)
+
