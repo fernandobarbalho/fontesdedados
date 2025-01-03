7fbaba6bafe3aad4d6cd6a604c2cf95c783aff59
Fernando Barbalho <fbarbalho@hotmail.com>
2025-01-03 09:18:24 -0300

    Correções no ETL

--- ai_prepardness_index.R
+++ ai_prepardness_index.R
@@ -29,19 +29,35 @@ indice_preparo_ia<- trata_tabela("imf-dm-export-20250102.xls", "preparo_ia" )
 infra_estrutura_digital<- trata_tabela("imf-dm-export-20250102(1).xls", "infraestrutura_digital")
 
 
+#trata índice de capital humano
+capital_humano<- trata_tabela("imf-dm-export-20250102(4).xls", "capital_humano")
+
+
 #Importa índice de inovação e integração econômica
 inovacao_integracao_ecomomica<- trata_tabela("imf-dm-export-20250102(2).xls", "inovacao_integracao_ecomomica")
 
 
 #Importa índice de regulação e ética
-regulacao_etica<- trata_tabela("imf-dm-export-20250102(2).xls", "regulacao_etica")
+regulacao_etica<- trata_tabela("imf-dm-export-20250102(3).xls", "regulacao_etica")
+
+
 
 consolidado_preparo_ia<-
   bind_rows(
     indice_preparo_ia,
     infra_estrutura_digital,
+    capital_humano,
     inovacao_integracao_ecomomica,
     regulacao_etica
   )
 
 
+consolidado_preparo_ia %>%
+  ggplot(aes(x= tipo_indice, y = indice)) +
+  geom_boxplot()
+
+consolidado_preparo_ia %>%
+  readr::write_csv("dados_preparedness_to_ia.csv")
+
+consolidado_preparo_ia %>%
+  filter(pais == "Brazil")
