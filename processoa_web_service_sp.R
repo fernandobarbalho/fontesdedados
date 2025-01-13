4d09556a7d7486aab6523ca8ae97e149d4e58d24
Fernando Barbalho <fbarbalho@hotmail.com>
2025-01-13 16:03:25 -0300

    GEração de dataframe a partir de xml

--- processoa_web_service_sp.R
+++ processoa_web_service_sp.R
@@ -21,7 +21,10 @@ soap_body <- sprintf('
   </soap:Header>
   <soap:Body>
     <ConsultarDespesas xmlns="http://fazenda.sp.gov.br/wstransparencia">
-      <ano>2024</ano>
+      <ano>2010</ano>
+      <codigoOrgao>20000</codigoOrgao>
+      <codigoGrupo>31</codigoGrupo>
+      <codigoModalidade>3190</codigoModalidade>
       <flagEmpenhado>0</flagEmpenhado>
       <flagLiquidado>0</flagLiquidado>
       <flagPago>1</flagPago>
@@ -57,28 +60,75 @@ if (status_code(response) == 200) {
 # Parsear o XML
 xml_parsed <- read_xml(xml_response)
 
-# Visualizar a estrutura do XML (ajuda a identificar os nós que contêm os dados)
-cat(as.character(xml_parsed))
 
+
+# Visualizar a estrutura do XML (ajuda a identificar os nós que contêm os dados)
+cat(as.character(xml_parsed),file= "saida.xml")
+
+
+
+# Leia o arquivo XML
+arquivo_xml <- "saida.xml"
+
+# Carregue o XML
+doc <- read_xml(arquivo_xml)
+
+# Defina o namespace utilizado no arquivo
+ns <- xml_ns(doc)
+
+# Substitua "d1" pelo namespace real encontrado no arquivo
+ns <- c(d1 = "http://fazenda.sp.gov.br/wstransparencia")
+
+# Extraia os nós <ItemDespesa> dentro do namespace
+itens_despesa <- xml_find_all(doc, ".//d1:ItemDespesa", ns = ns)
+
+# Extraia os dados relevantes e crie um dataframe
+dados <- tibble(
+  CodigoNomeOrgao = xml_text(xml_find_first(itens_despesa, ".//d1:CodigoNomeOrgao")),
+  CodigoNomeTipoLicitacao = xml_text(xml_find_first(itens_despesa, ".//d1:CodigoNomeTipoLicitacao")),
+  NaturezaDespesaNomeItem = xml_text(xml_find_first(itens_despesa, ".//d1:NaturezaDespesaNomeItem")),
+  ValorEmpenhado = xml_text(xml_find_first(itens_despesa, ".//d1:ValorEmpenhado")),
+  ValorLiquidado = xml_text(xml_find_first(itens_despesa, ".//d1:ValorLiquidado")),
+  ValorPago = xml_text(xml_find_first(itens_despesa, ".//d1:ValorPago")),
+  ValorPagoAnosAnteriores = xml_text(xml_find_first(itens_despesa, ".d1://ValorPagoAnosAnteriores"))
+)
+
+# Limpeza dos valores numéricos (removendo espaços e formatando)
+dados <- dados %>%
+  mutate(
+    ValorEmpenhado = as.numeric(gsub("[^0-9,]", "", ValorEmpenhado)),
+    ValorLiquidado = as.numeric(gsub("[^0-9,]", "", ValorLiquidado)),
+    ValorPago = as.numeric(gsub("[^0-9,]", "", ValorPago)),
+    ValorPagoAnosAnteriores = as.numeric(gsub("[^0-9,]", "", ValorPagoAnosAnteriores))
+  )
+
+
+
+
+#####################Tentativas anteriores
 # Extrair os dados específicos do XML
 # Suponha que os dados estão em elementos <Item>
 # Ajuste os nomes dos nós conforme o XML retornado pelo serviço
-data_nodes <- xml_find_all(xml_parsed, ".//ItemDespesa")
 
-data_nodes<- xml_child(xml_child(xml_child(xml_child(xml_child(xml_parsed, 1), 1), 1), 3), 1)
+# data_nodes <- xml_find_all(xml_parsed, ".//ItemDespesa")
+# 
+# data_nodes<- xml_child(xml_child(xml_child(xml_child(xml_child(xml_parsed, 1), 1), 1), 3), 1)
+# 
+# # Converter os dados para uma lista ou tibble
+# data_list <- data_nodes %>%
+#   map(~ as.list(xml_attrs(.)) %>% as_tibble())
+# 
+# # Combinar todos os itens em um dataframe
+# df <- bind_rows(data_list)
+# 
+# # Converter para data.table (opcional)
+# dt <- as.data.table(df)
+# 
+# # Visualizar o resultado
+# print(df)
 
-# Converter os dados para uma lista ou tibble
-data_list <- data_nodes %>%
-  map(~ as.list(xml_attrs(.)) %>% as_tibble())
 
-# Combinar todos os itens em um dataframe
-df <- bind_rows(data_list)
 
-# Converter para data.table (opcional)
-dt <- as.data.table(df)
-
-# Visualizar o resultado
-print(df)
 
 
 <ano>2024</ano>
