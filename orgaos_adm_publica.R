384a6b03d743415b003a81763f2a4c6f697a5639
Fernando Barbalho <fbarbalho@hotmail.com>
2025-01-29 15:45:17 -0300

    tratamento de todos os campos importantes do xml

--- orgaos_adm_publica.R
+++ orgaos_adm_publica.R
@@ -1,39 +1,73 @@
 library(jsonlite)
+library(tidyverse)
 
 
-url<- "https://estruturaorganizacional.dados.gov.br/doc/orgao-entidade/resumida?codigoPoder=1&codigoEsfera=1"
+
+
+trata_xml<- function(url){ #tipo_campo (1-código, 2-valor)
+  
+  map_dfr(unique(url), function(a_url){
+    list_codigo<- jsonlite::fromJSON(a_url)
+    df<- list_codigo[[2]]
+    df<- df[,1:2]
+    df$url <- a_url
+    names(df)[3]<- paste0("url_", names(df)[1])
+    df
+  })
+  
+}
+
+trata_xml_unidade_pai <- function(url){ #tipo_campo (1-código, 2-valor)
+  
+  map_dfr(unique(url), function(a_url){
+    list_codigo<- jsonlite::fromJSON(a_url)
+    df<- list_codigo[[2]]
+    tibble(url_codigoUnidadePai= a_url, sigla_unidade_pai = df$sigla, nome_unidade_pai = df$nome )
+  
+  })
+  
+}
+
+
+
 
 orgaos<- jsonlite::fromJSON(url)
 
 df_orgaos<- orgaos[["unidades"]]
 
-df_orgaos$codigoNaturezaJuridica
+df_orgaos_trabalho<-
+  df_orgaos %>%
+  rename_with( ~paste0("url_",.x), starts_with("codigo"))
+
+codigos_natureza<-
+  trata_xml(df_orgaos_trabalho$url_codigoNaturezaJuridica)
+
+codigo_poder<-
+  trata_xml(df_orgaos_trabalho$url_codigoPoder)
+
+codigo_TipoUnidade<-
+  trata_xml(df_orgaos_trabalho$url_codigoTipoUnidade)
+
+codigo_esfera<-
+  trata_xml(df_orgaos_trabalho$url_codigoEsfera)
+
+unidade_pai<-
+  trata_xml_unidade_pai(df_orgaos_trabalho$url_codigoUnidadePai)
+
+colunas_sel<- names(df_orgaos_trabalho)[str_detect(names(df_orgaos_trabalho), "url")]
 
 
-https://estruturaorganizacional.dados.gov.br/id/natureza-juridica/4
+orgaos_processados<-
+df_orgaos_trabalho %>%
+  inner_join(codigos_natureza) %>%
+  inner_join(codigo_poder) %>%
+  inner_join(codigo_TipoUnidade) %>%
+  inner_join(codigo_esfera) %>%
+  inner_join(unidade_pai) %>%
+  select(-all_of(colunas_sel))
 
 
 
-df_orgaos %>%
-  select(nome, sigla, codigoNaturezaJuridica) %>%
-  mutate(codigoNaturezaJuridica = substr(codigoNaturezaJuridica, str_length(codigoNaturezaJuridica), str_length(codigoNaturezaJuridica))) %>%
-  distinct(codigoNaturezaJuridica)
 
 
-df_orgaos %>%
-  select(nome, sigla, codigoNaturezaJuridica) %>%
-  mutate(codigoNaturezaJuridica = substr(codigoNaturezaJuridica, str_length(codigoNaturezaJuridica), str_length(codigoNaturezaJuridica))) %>%
-  mutate(natureza_juridica = case_when(
-    codigoNaturezaJuridica == "1" ~"Empresa Pública",
-    codigoNaturezaJuridica == "2" ~"Fundação Pública", 
-    codigoNaturezaJuridica == "3" ~"Administração Direta",
-    codigoNaturezaJuridica == "4" ~"Autarquia",
-    codigoNaturezaJuridica == "6" ~"Sociedade de Economia Mista",
-    codigoNaturezaJuridica == "7" ~"Órgão Público Autônomo Federal"
-    
-  )) %>%
-  writexl::write_xlsx("orgaos_adm_publica.xlsx")
 
-  
-
-substr("https://estruturaorganizacional.dados.gov.br/id/natureza-juridica/4", str_length("https://estruturaorganizacional.dados.gov.br/id/natureza-juridica/4"), str_length("https://estruturaorganizacional.dados.gov.br/id/natureza-juridica/4"))
