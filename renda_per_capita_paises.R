96a90d10448dc53d02cd016f43f2bda682cc34ad
Fernando Almeida Barbalho <40247971+fernandobarbalho@users.noreply.github.com>
2025-10-09 18:43:34 -0300

    dados sobre renda per-capita dos países

--- renda_per_capita_paises.R
+++ renda_per_capita_paises.R
@@ -1,6 +1,10 @@
 library(readxl)
 library(tidyverse)
 
+classes_renda <- 
+  read_excel("CLASS_2025_10_07.xlsx") %>%
+  janitor::clean_names()
+
 gdp_per_capita_worldbank <- 
   read_excel("gdp-per-capita-worldbank.xlsx") %>%
   janitor::clean_names() %>%
@@ -11,22 +15,33 @@ gdp_per_capita_worldbank <-
 
 dados_grafico <-
   gdp_per_capita_worldbank %>%
-  mutate(escopo = "mundo")
+  mutate(escopo = "mundo") %>%
+  inner_join(classes_renda) %>%
+  rename(regiao_wb = region  )
+
 
 dados_grafico_brasil<-
   dados_grafico %>%
-  filter(code == "BRA")
+  filter(code == "BRA") 
+
 
 dados_grafico %>%
   ggplot(aes(x= escopo, y= value)) +
-  geom_boxplot(outliers = FALSE) +
+  geom_boxplot(outlier.shape = "") +
   geom_jitter() +
-  geom_jitter(data = dados_grafico_brasil, color = "red")
+  geom_jitter(data = dados_grafico_brasil, color = "red", size =3)
 
 
 
 dados_grafico %>%
   ggplot(aes(x= regiao, y= value)) +
-  geom_boxplot(outliers = FALSE) +
+  geom_boxplot(outlier.shape = "") +
   geom_jitter() +
-  geom_jitter(data = dados_grafico_brasil, color = "red")
+  geom_jitter(data = dados_grafico_brasil, color = "red", size = 2)
+
+
+dados_grafico %>%
+  ggplot(aes(x= income_group, y= value)) +
+  geom_boxplot(outlier.shape = "") +
+  geom_jitter() +
+  geom_jitter(data = dados_grafico_brasil, color = "red", size = 2)
